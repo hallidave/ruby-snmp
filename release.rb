d1796248f8bf74434967a5065a3dfbb276b0abd8
davehal <davehal@dab43287-7d07-0410-bf85-a75ee65a218d>
2006-05-05 00:56:11 +0000

    Changed version on release script

--- release.rb
+++ release.rb
@@ -1,8 +1,18 @@
 system "rake test"
 exit if $? != 0
 
-RELEASE="1.0.0"
+RELEASE="1.0.1"
 TAG_CMD="svn copy svn+ssh://davehal@rubyforge.org/var/svn/snmplib/branches/branch-0.6.0 svn+ssh://davehal@rubyforge.org/var/svn/snmplib/tags/release-#{RELEASE} -m 'Tagging release #{RELEASE}'"
+
+def check_svn_up
+    puts "Checking svn updated"
+    out = `svn up`
+    unless out =~ /At revision \d+\./
+        puts "svn up failed:"
+        puts out
+        exit(1)
+    end
+end
 
 def check_svn_st
     puts "Checking svn committed"
@@ -13,16 +23,6 @@ def check_svn_st
             puts out
             exit(1)
         end
-    end
-end
-
-def check_svn_up
-    puts "Checking svn updated"
-    out = `svn up`
-    unless out =~ /At revision \d+\./
-        puts "svn up failed:"
-        puts out
-        exit(1)
     end
 end
 
